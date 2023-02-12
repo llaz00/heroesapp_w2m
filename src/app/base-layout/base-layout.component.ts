9b6c457c20f583fb8b5e23c4fe0de70cd26bc261
Luis Lazaro <llazarocausape@gmail.com>
2023-02-12 22:26:28 +0100

    Fixed responsive issues

--- src/app/base-layout/base-layout.component.ts
+++ src/app/base-layout/base-layout.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
     selector: 'app-base-layout',
     templateUrl: './base-layout.component.html',
     styleUrls: ['./base-layout.component.scss'],
 })
-export class BaseLayoutComponent {}
+export class BaseLayoutComponent {
+    isSidebarOpened = true;
+    isMobile = false;
+
+    constructor() {
+        if (window.innerWidth <= 600) {
+            this.isSidebarOpened = this.isSidebarOpened = false;
+            this.updateIsMobile();
+        }
+    }
+
+    @HostListener('window:resize', ['$event'])
+    updateIsMobile(): void {
+        this.isMobile = window.innerWidth <= 600;
+    }
+}
